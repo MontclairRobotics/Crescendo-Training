1af85286f91f6286f4b83038bce89395d423bb98
Jame <jctee02@gmail.com>
2024-05-02 06:45:28 -0400

    Sprocket without the Sprocket
    :l

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,16 +10,18 @@ import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
-
+import frc.robot.subsystems.Sprocket;
+import frc.robot.util.ControllerTools;
+import frc.robot.util.ControllerTools.DPad;
 public class RobotContainer {
   //pissing james rechs off by doing indents
     public static Intake intake = new Intake();
+    public      static Sprocket sprocket = new Sprocket();
     public static     Shooter shooter = new Shooter();
     public      static CommandPS5Controller driverController = new CommandPS5Controller(0);
-   public static       CommandPS5Controller operatorController = new CommandPS5Controller(1);
+    public static       CommandPS5Controller operatorController = new CommandPS5Controller(1);
     public     static CommandPS5Controller testingController = new CommandPS5Controller(2);
     public Drivetrain drivetrain = new Drivetrain();
-
   public RobotContainer() {
 
     
@@ -35,7 +37,8 @@ public class RobotContainer {
     operatorController.circle().onTrue(RobotContainer.shooter.SysIDCommand);
     operatorController.L1().onTrue(RobotContainer.intake.inhaleCommand());
     operatorController.R1().onTrue(RobotContainer.intake.exhaleCommand());
-    
+    ControllerTools.getDPad(DPad.UP, driverController).onTrue(RobotContainer.sprocket.setAngleCommand(63));
+    ControllerTools.getDPad(DPad.DOWN, driverController).onTrue(RobotContainer.sprocket.setAngleCommand(26));
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/Sprocket.java
@@ -0,0 +1,44 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants;
+
+public class Sprocket {
+    static CANSparkMax leftSprocketMotor = new CANSparkMax(Constants.Ports.LEFT_ANGLE_MOTOR, MotorType.kBrushless);
+    static CANSparkMax rightSprocketMotor = new CANSparkMax(Constants.Ports.RIGHT_ANGLE_MOTOR, MotorType.kBrushless);
+    
+    //TODO: Actually Use These
+    RelativeEncoder leftEncoder = leftSprocketMotor.getEncoder(); 
+    RelativeEncoder rightEncoder = rightSprocketMotor.getEncoder(); 
+
+    static PIDController pidController = new PIDController(1,1,1); 
+    
+    public static void SprocketUp(){
+    } 
+    public static void SprocketDown(){
+    }
+    public static void setAngle(Rotation2d angle){
+        pidController.setSetpoint(angle.getDegrees());
+    }
+    
+    public static void stop(){
+        rightSprocketMotor.set(0);
+        leftSprocketMotor.set(0);
+    }
+    public Command setAngleCommand(double angle){
+        return Commands.runOnce(() -> Sprocket.setAngle(Rotation2d.fromDegrees(angle)));
+    }
+    public Command stopSprocket(){
+        return Commands.runOnce(() -> Sprocket.stop());
+    }
+
+    public void periodic(){
+    }
+}
