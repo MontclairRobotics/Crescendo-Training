8dcea964d2773f4f2b1f9b940519a1cb2a953d94
Jame <jctee02@gmail.com>
2024-05-02 17:33:13 -0400

    Sprocket *should* move now
    = )

--- src/main/java/frc/robot/subsystems/Sprocket.java
+++ src/main/java/frc/robot/subsystems/Sprocket.java
@@ -8,21 +8,26 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Sprocket {
+public class Sprocket extends SubsystemBase{
     static CANSparkMax leftSprocketMotor = new CANSparkMax(Constants.Ports.LEFT_ANGLE_MOTOR, MotorType.kBrushless);
     static CANSparkMax rightSprocketMotor = new CANSparkMax(Constants.Ports.RIGHT_ANGLE_MOTOR, MotorType.kBrushless);
     
     //TODO: Actually Use These
-    RelativeEncoder leftEncoder = leftSprocketMotor.getEncoder(); 
-    RelativeEncoder rightEncoder = rightSprocketMotor.getEncoder(); 
+    static RelativeEncoder leftEncoder = leftSprocketMotor.getEncoder(); 
+    static RelativeEncoder rightEncoder = rightSprocketMotor.getEncoder(); 
 
     static PIDController pidController = new PIDController(1,1,1); 
     
     public static void SprocketUp(){
+        leftSprocketMotor.set(pidController.calculate(leftEncoder.getPosition()));
+        rightSprocketMotor.set(pidController.calculate(rightEncoder.getPosition()));
     } 
     public static void SprocketDown(){
+        leftSprocketMotor.set(-pidController.calculate(leftEncoder.getPosition()));
+        rightSprocketMotor.set(-pidController.calculate(rightEncoder.getPosition()));
     }
     public static void setAngle(Rotation2d angle){
         pidController.setSetpoint(angle.getDegrees());
