efc0b4d1c7fc4eef003e322663996e17ceb1911b
cbgpenguin <cbaneygiampoala@gmail.com>
2024-05-06 13:47:11 -0400

    Added a start and  stop method for the Climber Motors
    Probably did it wrong and I'm not really sure what I'm doing.

--- src/main/java/frc/robot/subsystems/Climbers.java
+++ src/main/java/frc/robot/subsystems/Climbers.java
@@ -9,4 +9,16 @@ import frc.robot.Constants;
 public class Climbers extends SubsystemBase{
     CANSparkMax leftClimberMotor = new CANSparkMax(Constants.Ports.CLIMBER_LEFT_MOTOR,MotorType.kBrushless);
     CANSparkMax rightClimberMotor = new CANSparkMax(Constants.Ports.CLIMBER_RIGHT_MOTOR,MotorType.kBrushless);
+
+    //start climber motors
+    public void start(){
+        leftClimberMotor.set(Constants.ClimberConstants.CLIMBER_SPEED);
+        rightClimberMotor.set(Constants.ClimberConstants.CLIMBER_SPEED);
+    }
+    //stop the climber motors (I dont think I did this right)
+    public void stop(){
+        leftClimberMotor.set(0);
+        rightClimberMotor.set(0);
+    }
+    //todo: firger out what I'm supossed to be doing (also figer out if I'm spelling figur right)
 }
